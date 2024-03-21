91912f5f6579dc73729dce7353506b0fb8a53002
Andrian Putra Ramadan <ramadanandrian89@gmail.com>
2024-03-21 23:02:49 +0700

    feat: add story point per issue

--- src/IssueHandler.ts
+++ src/IssueHandler.ts
@@ -1,209 +1,245 @@
 import JiraClient from "./JiraClient";
 
-interface Issue {
-  id: string;
-  key: string;
-  project: string;
-  isResolved: boolean;
-  inProgressAt: string | null;
-  inReviewAt: string | null;
-  createdAt: string;
-  resolvedAt: string;
-  status: string;
-  stage: {
+interface Stage {
     createToInProgress: number;
     inProgressToInReview: number;
     inReviewToDone: number;
     createToInReview: number;
     createToDone: number;
-    averageDuration: number;
-  };
 }
 
+interface Issue {
+    id: string;
+    key: string;
+    project: string;
+    summary: string;
+    isResolved: boolean;
+    inProgressAt: string | null;
+    inReviewAt: string | null;
+    createdAt: string;
+    resolvedAt: string;
+    status: string;
+    estimatedStoryPoint: number;
+    stage: Stage;
+}
+
+
 interface IssueStatusHistory {
-  created: string;
-  status: "In Progress" | "In Review" | "Done" | "To Do";
+    created: string;
+    status: "In Progress" | "In Review" | "Done" | "To Do";
 }
 
 interface User {
-  displayName: string;
-  accountId: string;
-  accountType: string;
-  active: boolean;
-  emailAddress: string;
-  issues?: Issue[];
+    displayName: string;
+    accountId: string;
+    accountType: string;
+    active: boolean;
+    emailAddress: string;
+    issues?: Issue[];
+    averageStageDuration: Stage | null;
 }
 
 export default class IssueHandler {
-  constructor(private jiraClient: JiraClient) {}
+    constructor(private jiraClient: JiraClient) {
+    }
 
-  /**
-   * Retrieves and processes issues assigned to a specific account ID.
-   *
-   * @param {string} assigneeAccountId
-   * @returns {Promise<Issue[]>}
-   */
-  private async processIssues(assigneeAccountId: string): Promise<Issue[]> {
-    const issues: Issue[] = [];
+    /**
+     * Retrieves and processes issues assigned to a specific account ID.
+     *
+     * @param {string} assigneeAccountId
+     * @returns {Promise<Issue[]>}
+     */
+    private async processIssues(assigneeAccountId: string): Promise<Issue[]> {
+        const issues: Issue[] = [];
 
-    let startAt = 0;
-    const maxResults = 50;
+        let startAt = 0;
+        const maxResults = 50;
 
-    while (true) {
-      try {
-        const response = await this.jiraClient.getIssues(
-          assigneeAccountId,
-          startAt,
-          maxResults,
-        );
+        while (true) {
+            try {
+                const response = await this.jiraClient.getIssues(
+                    assigneeAccountId,
+                    startAt,
+                    maxResults,
+                );
 
-        if (response.issues.length === 0) break;
+                if (response.issues.length === 0) break;
 
-        for (const issue of response.issues) {
-          if (issue.fields.resolutiondate) {
-            const statusHistory = await this.jiraClient.getIssueStatusHistory(
-              issue.id,
-            );
-            const stage = this.getStage(issue, statusHistory);
+                for (const issue of response.issues) {
+                    if (issue.fields.resolutiondate) {
+                        const statusHistory = await this.jiraClient.getIssueStatusHistory(
+                            issue.id,
+                        );
+                        const stage = this.getStage(issue, statusHistory);
 
-            const inProgressTimestamp =
-              this.getStatusTimestamp("In Progress", statusHistory)?.created ||
-              null;
+                        const inProgressTimestamp =
+                            this.getStatusTimestamp("In Progress", statusHistory)?.created ||
+                            null;
 
-            const inReviewTimestamp =
-              this.getStatusTimestamp("In Review", statusHistory)?.created ||
-              null;
+                        const inReviewTimestamp =
+                            this.getStatusTimestamp("In Review", statusHistory)?.created ||
+                            null;
 
-            issues.push({
-              id: issue.id,
-              key: issue.key,
-              project: issue.fields.project.name,
-              isResolved: !!issue.fields.resolution,
-              inProgressAt: inProgressTimestamp,
-              inReviewAt: inReviewTimestamp,
-              createdAt: issue.fields.created,
-              resolvedAt: issue.fields.resolutiondate,
-              status: issue.fields.status.name,
-              stage,
-            });
-          }
+                        const estimatedStoryPointField = `customfield_${this.jiraClient.config.storyPointCustomFieldId}`;
+
+                        issues.push({
+                            id: issue.id,
+                            key: issue.key,
+                            project: issue.fields.project.name,
+                            summary: issue.fields.summary,
+                            isResolved: !!issue.fields.resolution,
+                            inProgressAt: inProgressTimestamp,
+                            inReviewAt: inReviewTimestamp,
+                            createdAt: issue.fields.created,
+                            resolvedAt: issue.fields.resolutiondate,
+                            status: issue.fields.status.name,
+                            estimatedStoryPoint: issue.fields[estimatedStoryPointField],
+                            stage
+                        });
+                    }
+                }
+
+                startAt += maxResults;
+            } catch (error) {
+                console.error(error);
+            }
         }
 
-        startAt += maxResults;
-      } catch (error) {
-        console.error(error);
-      }
+        return issues;
     }
 
-    return issues;
-  }
+    /**
+     * Retrieves users from Jira and processes their issues.
+     *
+     * @returns {Promise<User[]>}
+     */
+    public async execute(): Promise<User[]> {
+        const users = await this.jiraClient.getUsers();
 
-  /**
-   * Retrieves users from Jira and processes their issues.
-   *
-   * @returns {Promise<User[]>}
-   */
-  public async execute(): Promise<User[]> {
-    const users = await this.jiraClient.getUsers();
+        for (const user of users) {
+            user.issues = await this.processIssues(user.accountId);
+            user.averageStageDuration = this.countAverageStageDuration(user.issues);
+        }
 
-    for (const user of users)
-      user.issues = await this.processIssues(user.accountId);
-
-    return users;
-  }
-
-  private getStage(
-    issue: any,
-    statusHistory: IssueStatusHistory[],
-  ): Issue["stage"] {
-    const timestamp = {
-      inProgress:
-        this.getStatusTimestamp("In Progress", statusHistory)?.created || null,
-      inReview:
-        this.getStatusTimestamp("In Review", statusHistory)?.created || null,
-      done: issue.fields.resolutiondate,
-      created: issue.fields.created,
-    };
-
-    const stage = {
-      createToInProgress: this.countTimeDifference(
-        timestamp.created,
-        timestamp.inProgress,
-      ),
-      inProgressToInReview: this.countTimeDifference(
-        timestamp.inProgress,
-        timestamp.inReview,
-      ),
-      inReviewToDone: this.countTimeDifference(
-        timestamp.inReview,
-        timestamp.done,
-      ),
-      createToInReview: this.countTimeDifference(
-        timestamp.created,
-        timestamp.inReview,
-      ),
-      createToDone: this.countTimeDifference(timestamp.created, timestamp.done),
-    };
-
-    const averageDuration = this.countStageAverage(stage);
-
-    return { ...stage, averageDuration };
-  }
-
-  private countStageAverage(
-    stage: Omit<Issue["stage"], "averageDuration">,
-  ): number {
-    const timeDifferences = Object.values(stage);
-    const sum = timeDifferences.reduce((acc, val) => acc + val, 0);
-    return sum / timeDifferences.length;
-  }
-
-  /**
-   * Count the time difference between two dates in second.
-   *
-   * @param start
-   * @param end
-   * @private
-   */
-  private countTimeDifference(
-    start: string | null,
-    end: string | null,
-  ): number {
-    if (start === null || end === null) return 0;
-
-    const startTime = new Date(start).getTime();
-    const endTime = new Date(end).getTime();
-
-    return Math.floor((endTime - startTime) / 1000);
-  }
-
-  /**
-   *
-   * @param status
-   * @param statusHistory
-   * @private
-   */
-  private getStatusTimestamp(
-    status: "In Progress" | "In Review",
-    statusHistory: IssueStatusHistory[],
-  ) {
-    let index: number;
-
-    switch (status) {
-      case "In Progress":
-        index = statusHistory.findIndex(
-          (status) => status.status === "In Progress",
-        );
-        break;
-      case "In Review":
-        index = statusHistory.findLastIndex(
-          (status) => status.status === "In Review",
-        );
-        break;
-      default:
-        return null;
+        return users;
     }
 
-    return index > -1 ? statusHistory[index] : null;
-  }
+    private getStage(
+        issue: any,
+        statusHistory: IssueStatusHistory[],
+    ): Stage {
+        const timestamp = {
+            inProgress:
+                this.getStatusTimestamp("In Progress", statusHistory)?.created || null,
+            inReview:
+                this.getStatusTimestamp("In Review", statusHistory)?.created || null,
+            done: issue.fields.resolutiondate,
+            created: issue.fields.created,
+        };
+
+        return {
+            createToInProgress: this.countTimeDifference(
+                timestamp.created,
+                timestamp.inProgress,
+            ),
+            inProgressToInReview: this.countTimeDifference(
+                timestamp.inProgress,
+                timestamp.inReview,
+            ),
+            inReviewToDone: this.countTimeDifference(
+                timestamp.inReview,
+                timestamp.done,
+            ),
+            createToInReview: this.countTimeDifference(
+                timestamp.created,
+                timestamp.inReview,
+            ),
+            createToDone: this.countTimeDifference(timestamp.created, timestamp.done),
+        };
+    }
+
+    /**
+     * Count the time difference between two dates in second.
+     *
+     * @param start
+     * @param end
+     * @private
+     */
+    private countTimeDifference(
+        start: string | null,
+        end: string | null,
+    ): number {
+        if (start === null || end === null) return 0;
+
+        const startTime = new Date(start).getTime();
+        const endTime = new Date(end).getTime();
+
+        return Math.floor((endTime - startTime) / 1000);
+    }
+
+    /** Get timestamp for specified status.
+     *
+     * @param status
+     * @param statusHistory
+     * @private
+     */
+    private getStatusTimestamp(
+        status: "In Progress" | "In Review",
+        statusHistory: IssueStatusHistory[],
+    ) {
+        let index: number;
+
+        switch (status) {
+            case "In Progress":
+                index = statusHistory.findIndex(
+                    (status) => status.status === "In Progress",
+                );
+                break;
+            case "In Review":
+                index = statusHistory.findLastIndex(
+                    (status) => status.status === "In Review",
+                );
+                break;
+            default:
+                return null;
+        }
+
+        return index > -1 ? statusHistory[index] : null;
+    }
+
+    /**
+     * Count the average stage duration.
+     *
+     *
+     * @param issues
+     * @private
+     */
+    private countAverageStageDuration(issues: Issue[]): Stage {
+        const stageDurationCount = {
+            createToInProgress: 0,
+            inProgressToInReview: 0,
+            inReviewToDone: 0,
+            createToInReview: 0,
+            createToDone: 0
+        };
+
+        issues.forEach((issue) => {
+            const stage = issue.stage;
+            stageDurationCount.createToInProgress += stage.createToInProgress;
+            stageDurationCount.inProgressToInReview += stage.inProgressToInReview;
+            stageDurationCount.inReviewToDone += stage.inReviewToDone;
+            stageDurationCount.createToInReview += stage.createToInReview;
+            stageDurationCount.createToDone += stage.createToDone;
+        });
+
+        const totalIssue = issues.length;
+
+        return {
+            createToInProgress: Math.floor(stageDurationCount.createToInProgress / totalIssue),
+            inProgressToInReview: Math.floor(stageDurationCount.inProgressToInReview / totalIssue),
+            inReviewToDone: Math.floor(stageDurationCount.inReviewToDone / totalIssue),
+            createToInReview: Math.floor(stageDurationCount.createToInReview / totalIssue),
+            createToDone: Math.floor(stageDurationCount.createToDone / totalIssue),
+        }
+    }
 }
--- src/JiraClient.ts
+++ src/JiraClient.ts
@@ -1,149 +1,163 @@
 import axios from "axios";
 
 interface JiraConfig {
-  jiraApiUrl: string;
-  jiraApiToken: string;
-  jiraEmail: string;
+    jiraApiUrl: string;
+    jiraApiToken: string;
+    jiraEmail: string;
+    storyPointCustomFieldId: string;
 }
 
 interface Option {
-  since: string;
-  until: string;
+    since: string;
+    until: string;
 }
 
 interface User {
-  displayName: string;
-  accountId: string;
-  accountType: string;
-  active: boolean;
-  emailAddress: string;
-  issues?: Issue[];
+    displayName: string;
+    accountId: string;
+    accountType: string;
+    active: boolean;
+    emailAddress: string;
+    issues?: Issue[];
+    averageStageDuration: Stage | null;
 }
 
-interface Issue {
-  id: string;
-  key: string;
-  project: string;
-  isResolved: boolean;
-  inProgressAt: string | null;
-  inReviewAt: string | null;
-  createdAt: string;
-  resolvedAt: string;
-  status: string;
-  stage: {
+interface Stage {
     createToInProgress: number;
     inProgressToInReview: number;
     inReviewToDone: number;
     createToInReview: number;
     createToDone: number;
-    averageDuration: number;
-  };
+}
+
+interface Issue {
+    id: string;
+    key: string;
+    project: string;
+    summary: string;
+    isResolved: boolean;
+    inProgressAt: string | null;
+    inReviewAt: string | null;
+    createdAt: string;
+    resolvedAt: string;
+    status: string;
+    stage: Stage;
+    estimatedStoryPoint: number;
 }
 
 interface IssueStatusHistory {
-  created: string;
-  status: "In Progress" | "In Review" | "Done" | "To Do";
+    created: string;
+    status: "In Progress" | "In Review" | "Done" | "To Do";
 }
 
 export default class JiraClient {
-  private readonly authToken: string;
+    private readonly authToken: string;
 
-  constructor(
-    private readonly config: JiraConfig,
-    private readonly option: Option,
-  ) {
-    this.authToken = Buffer.from(
-      `${config.jiraEmail}:${config.jiraApiToken}`,
-    ).toString("base64");
-  }
+    constructor(
+        private readonly jiraConfig: JiraConfig,
+        private readonly option: Option,
+    ) {
 
-  /**
-   * Retrieves all issues from Jira based on the assigneeAccountId.
-   *
-   * @param assigneeAccountId
-   * @param startAt
-   * @param maxResults
-   * @returns {Promise<any>} List of issues.
-   */
-  public async getIssues(
-    assigneeAccountId: string,
-    startAt: number = 0,
-    maxResults: number = 50,
-  ): Promise<any> {
-    try {
-      const response = await axios.get(
-        `${this.config.jiraApiUrl}/search?jql=created>=${this.option.since} and created<=${this.option.until} and assignee=${assigneeAccountId}&startAt=${startAt}&maxResults=${maxResults}`,
-        {
-          headers: {
-            Authorization: `Basic ${this.authToken}`,
-          },
-        },
-      );
-
-      return response.data;
-    } catch (error) {
-      console.error(error);
+        this.authToken = Buffer.from(
+            `${jiraConfig.jiraEmail}:${jiraConfig.jiraApiToken}`,
+        ).toString("base64");
     }
-  }
 
-  /**
-   * Retrieves a list of users from the Jira.
-   * Only users with the account type 'atlassian' are included in the result.
-   *
-   * @returns {Promise<User[]>} List of users.
-   */
-  public async getUsers(): Promise<User[]> {
-    const response = await axios.get(`${this.config.jiraApiUrl}/users`, {
-      headers: {
-        Authorization: `Basic ${this.authToken}`,
-      },
-    });
+    /**
+     * Retrieves all issues from Jira based on the assigneeAccountId.
+     *
+     * @param assigneeAccountId
+     * @param startAt
+     * @param maxResults
+     * @returns {Promise<any>} List of issues.
+     */
+    public async getIssues(
+        assigneeAccountId: string,
+        startAt: number = 0,
+        maxResults: number = 50,
+    ): Promise<any> {
+        try {
+            const response = await axios.get(
+                `${this.config.jiraApiUrl}/search?jql=created>=${this.option.since} and created<=${this.option.until} and assignee=${assigneeAccountId}&startAt=${startAt}&maxResults=${maxResults}`,
+                {
+                    headers: {
+                        Authorization: `Basic ${this.authToken}`,
+                    },
+                },
+            );
 
-    return response.data
-      .filter((user: User) => user.accountType === "atlassian" && user.active)
-      .map((user: User) => {
-        return {
-          displayName: user.displayName,
-          accountId: user.accountId,
-          accountType: user.accountType,
-          active: user.active,
-          emailAddress: user.emailAddress,
-        };
-      });
-  }
-
-  /**
-   * Retrieves the issue status history.
-   *
-   * @param issueId
-   * @returns {Promise<IssueStatusHistory[]>} List of issue status history.
-   */
-  public async getIssueStatusHistory(
-    issueId: string,
-  ): Promise<IssueStatusHistory[]> {
-    const response = await axios.get(
-      `${this.config.jiraApiUrl}/issue/${issueId}/changelog`,
-      {
-        headers: {
-          Authorization: `Basic ${this.authToken}`,
-        },
-      },
-    );
-
-    const status: IssueStatusHistory[] = [];
-
-    response.data.values.forEach((log: any) => {
-      const items = log.items;
-      items.forEach((item: any) => {
-        if (item.fieldId === "status") {
-          status.push({
-            created: log.created,
-            status: item.toString,
-          });
+            return response.data;
+        } catch (error) {
+            console.error(error);
         }
-      });
-    });
+    }
 
-    return status;
-  }
+    /**
+     * Retrieves a list of users from the Jira.
+     * Only users with the account type 'atlassian' are included in the result.
+     *
+     * @returns {Promise<User[]>} List of users.
+     */
+    public async getUsers(): Promise<User[]> {
+        const response = await axios.get(`${this.config.jiraApiUrl}/users`, {
+            headers: {
+                Authorization: `Basic ${this.authToken}`,
+            },
+        });
+
+        return response.data
+            .filter((user: User) => user.accountType === "atlassian" && user.active)
+            .map((user: User) => {
+                return {
+                    displayName: user.displayName,
+                    accountId: user.accountId,
+                    accountType: user.accountType,
+                    active: user.active,
+                    emailAddress: user.emailAddress,
+                };
+            });
+    }
+
+    /**
+     * Retrieves the issue status history.
+     *
+     * @param issueId
+     * @returns {Promise<IssueStatusHistory[]>} List of issue status history.
+     */
+    public async getIssueStatusHistory(
+        issueId: string,
+    ): Promise<IssueStatusHistory[]> {
+        const response = await axios.get(
+            `${this.config.jiraApiUrl}/issue/${issueId}/changelog`,
+            {
+                headers: {
+                    Authorization: `Basic ${this.authToken}`,
+                },
+            },
+        );
+
+        const status: IssueStatusHistory[] = [];
+
+        response.data.values.forEach((log: any) => {
+            const items = log.items;
+            items.forEach((item: any) => {
+                if (item.fieldId === "status") {
+                    status.push({
+                        created: log.created,
+                        status: item.toString,
+                    });
+                }
+            });
+        });
+
+        return status;
+    }
+
+
+    /**
+     * Getter for the Jira config.
+     */
+    public get config(): JiraConfig {
+        return this.jiraConfig;
+    }
 }
